6706812bebaa0e363c5fb91a2d89798107070d5c
sehoon6295 <930saehoon@gmail.com>
2024-11-03 20:44:35 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,19 +1,42 @@
 "use strict"
 
-const home = (req, res) => {
-    res.render('home/index');
+const output = {
+    home: (req, res) => {
+        res.render('home/index');
+    },
+    login: (req, res) => {
+        res.render('home/login');
+    },    
+    join: (req, res) => {
+        res.render('home/join');
+    },
 };
 
-const login = (req, res) => {
-    res.render('home/login');
+const users = {
+    id: ["sehoon6295", "sehoon629515"],
+    psword: ["sae1790!", "0930sehoon"],
 };
 
-const join = (req, res) => {
-    res.render('home/join');
-};
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
 
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === (psword)){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "잘못된 id 및 password입니다.",
+        });
+    }
+};
 module.exports = {
-    home,
-    login,
-    join,
+    output,
+    process,
 }
